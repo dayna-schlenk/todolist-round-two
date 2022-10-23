b4b5d92b324e7d6251a0e8d6ef24ea56bd1c4edd
dayna-schlenk <dayna.schlenker@gmail.com>
2022-10-23 11:52:45 -0500

    Create div and li in addTodo function

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ todoButton.addEventListener('click', addTodo);
 
 // functions
 function addTodo(event) {
+    // prevent form from submitting
     event.preventDefault();
-    console.log("helloooo");
+
+    // create div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'Hey?';
+    newTodo.classList.add('todo-item');
+
+    // append li to div
+    todoDiv.appendChild(newTodo);
+
+    // create button to delete
+    // create button to mark done
 }
