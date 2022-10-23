8422f2472642d0a9c306d15bd103c659fd40ea19
dayna-schlenk <dayna.schlenker@gmail.com>
2022-10-23 11:25:18 -0500

    Create button event listener with addTodo function

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 // event listeners
+todoButton.addEventListener('click', addTodo);
 
 // functions
+function addTodo(event) {
+    event.preventDefault();
+    console.log("helloooo");
+}
