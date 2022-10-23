72e831996d7a4a80137e0d48203905123e1dff5c
dayna-schlenk <dayna.schlenker@gmail.com>
2022-10-23 11:20:49 -0500

    Create js selectors

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
 // event listeners
 
